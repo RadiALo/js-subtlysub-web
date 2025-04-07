ced8d68366918f1bd42c68a2231625bf9bc0a7eb
RadiALo <78644882+RadiALo@users.noreply.github.com>
2025-04-07 10:40:38 +0300

    delete unused imports

--- subtlysub-frontend/src/App.tsx
+++ subtlysub-frontend/src/App.tsx
@@ -26,56 +26,67 @@ function App() {
           <Route path="/login" element={<LogIn />}/>
           <Route path="/signup" element={<SignUp />}/>
           <Route path="/verify" element={<Verify />}/>
+
           <Route path="/explore" element={
             <ProtectedRoute>
               <Explore />
             </ProtectedRoute>
           }/>
+
           <Route path="/home" element={
             <ProtectedRoute>
               <Home />
             </ProtectedRoute>
           }/>
+
           <Route path="/" element={
             <ProtectedRoute>
               <Home />
             </ProtectedRoute>
           }/>
+
           <Route path="/posts/create" element={
             <ProtectedRoute>
               <CreatePost />
             </ProtectedRoute>
           }/>
+
           <Route path="/posts/:id/edit" element={
             <ProtectedRoute>
               <EditPost />
             </ProtectedRoute>
           }/>
+
           <Route path="/posts/:id" element={
             <ProtectedRoute>
               <PostDetail />
             </ProtectedRoute>
           }/>
+
           <Route path="/collections/create" element={
             <ProtectedRoute>
               <CreateCollection />
             </ProtectedRoute>
           }/>
+
           <Route path="/collections/:id/edit" element={
             <ProtectedRoute>
               <EditCollection />
             </ProtectedRoute>
           }/>
+
           <Route path="/collections/:id" element={
             <ProtectedRoute>
               <CollectionDetail />
             </ProtectedRoute>
           }/>
+
           <Route path="/posts/:id/learn/app" element={
             <ProtectedRoute>
               <LearnApp />
             </ProtectedRoute>
           } />
+          
           <Route path="/posts/:id/learn" element={
             <ProtectedRoute>
               <Learn />
--- subtlysub-frontend/src/pages/Home.tsx
+++ subtlysub-frontend/src/pages/Home.tsx
@@ -1,72 +1,10 @@
-import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import PostItem from "../components/element/PostItem";
 import PostList from "../components/list/PostList";
-import { Post } from "../types/Post";
-import { Collection } from "../types/Collection";
-import CollectionItem from "../components/element/CollectionItem";
 import { useTranslation } from 'react-i18next';
 import CollectionList from "../components/list/CollectionList";
 
 const Home = () => {
   const { t } = useTranslation();
-
-
-  const apiUrl = import.meta.env.VITE_API_URL;
-  const token = localStorage.getItem("token");
   const user = JSON.parse(localStorage.getItem("user") || "{}");
-
-  const navigate = useNavigate();
-
-  const [myPosts, setMyPosts] = useState<Post[]>([]);
-
-  const [myCollections, setMyCollections] = useState<Collection[]>([]);
-
-  useEffect(() => {
-    const fetchPosts = async () => {
-      try {
-        const url = new URL(`${apiUrl}/api/posts`);
-        url.searchParams.append("authorId", user.id);
-        const response = await fetch(url.toString());
-    
-        if (!response.ok) {
-          throw new Error("Failed to fetch posts");
-        }
-        
-        const data = await response.json();
-        setMyPosts(data);
-      } catch (error) {
-        console.error("Error fetching tags: ", error);
-      }
-    }
-
-    fetchPosts();
-  }, [apiUrl]);
-
-  useEffect(() => {
-    const fetchCollections = async () => {
-      try {
-        const response = await fetch(`${apiUrl}/api/collections`, {
-          headers: {
-            Authorization: `Bearer ${token}`
-          }
-        });
-
-        if (!response.ok) {
-          throw new Error("Failed to fetch collections");
-        }
-
-        const data = await response.json();
-        setMyCollections(data);
-      } catch (error) {
-        console.error("Error fetching collections: ", error)
-      }
-    };
-
-    fetchCollections();
-  }, [apiUrl])
-
-
 
   return (
     <div className="max-w-5xl mx-auto p-6 flex flex-col gap-12">
